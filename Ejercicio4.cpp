1706cc92bfe771551c8e0f935ae49d76b2717305
Yair292 <dluquep@unjbg.edu.pe>
2024-07-15 11:05:46 -0500

    Mejorar la presentacion

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -6,13 +6,18 @@ int main(){
 	int opcion;
 	int var2;
 	int aux;
-
+	int matrizcopia[4][4];
 	int matriz[4][4]={
 		{1,2,3,4},
 		{5,6,7,8},
 		{9,10,11,12},
 		{13,14,15,16}
 	};
+for (int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
+		matrizcopia[i][j]=matriz[i][j];
+	}
+}
 	
 cout<<"Matriz Inicial:\n";
 for(int i=0;i<4;i++){
@@ -27,8 +32,12 @@ for(int i=0;i<4;i++){
 	matriz[var1][i]=matriz[var2][i];
 	matriz[var2][i]=aux;
 }
-cout<<"\nMatriz Cambiada:\n";
+cout<<"Matriz:\t\t\tMatriz Cambiada:\n";
 for(int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
+		cout<<matrizcopia[i][j]<<" ";
+	}
+	cout<<"\t\t";
 	for(int j=0;j<4;j++){
 		cout<<matriz[i][j]<<" ";
 	}
