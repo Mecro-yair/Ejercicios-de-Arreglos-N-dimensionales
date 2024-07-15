0c5ceb6747771749a654a86fb261209cf89b3708
Yair292 <dluquep@unjbg.edu.pe>
2024-07-14 23:19:41 -0500

    Agregar Ejercicio2:Buscar Valor de una matriz

---
+++ Ejercicio2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int valor;
+	int matriz[3][3]={
+		{1,2,3},
+		{4,2,6},
+		{7,2,9}
+		};
+	//mostrar matriz
+	cout<<"Matriz:\n";
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<matriz[i][j]<<" ";
+		}cout<<"\n";
+	}
+	cout<<"Valor a buscar: "; cin>>valor; cout<<"\n";
+	cout<<"El valor "<<valor<<" se encuentra en:\n";
+	
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if (valor==matriz[i][j]){
+				cout<<"Fila "<<i<<" ,Columna "<<j<<endl;
+			}
+		}
+	}
+	return 0;
+}
